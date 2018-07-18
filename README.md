ed8f1c9e30807a39caf1fab3d59b216be75d21a0
Andrew Law <andrewlaw111@gmail.com>
2018-07-18 18:19:11 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Profile Web Page
+Repository for profile webpage
---
+++ app.ts
@@ -0,0 +1,31 @@
+import * as express from 'express';
+import * as fs from 'fs';
+const app = express();
+
+let visitors: number;
+fs.readFile("./visitors.json", "utf8", (err, data) => {
+        if (err) {
+                visitors = 0;
+        } else {
+                const parsedData = JSON.parse(data)
+
+                visitors = parseInt(parsedData.visitors);
+        }
+})
+
+
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+        console.log(req.headers);
+        visitors++;
+        console.log("count: ", visitors);
+        fs.writeFile("./visitors.json", JSON.stringify({
+                "visitors": visitors
+        }), (err) => {
+                if (err) {
+                        console.log(err);
+                }
+        });
+        return res.sendFile(__dirname + "/index.html")
+})
+app.listen(8000, () => console.log('Listening on port 8000!'))
